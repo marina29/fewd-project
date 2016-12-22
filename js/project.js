405213c8faa07179331f7a00c16a5b180a92258b
marina29 <marina@MARINAs-MacBook.local>
2016-12-22 15:55:02 -0500

    committing project

--- js/project.js
+++ js/project.js
@@ -13,8 +13,8 @@ $("button").on("click", function() {
 			  method: 'GET',
 			}).done(function(result) {
 			  	
-			  	console.log(result["results"][0]["title"]);
-			  	// var i = 0
+			  	console.log(result["results"][4]["title"]);
+			  	
 				$("p#first").text(result["results"][0]["title"]);
 				$("p#second").text(result["results"][1]["title"]);
 				$("p#third").text(result["results"][2]["title"]);
@@ -22,33 +22,41 @@ $("button").on("click", function() {
 				$("p#fifth").text(result["results"][4]["title"]);
 
 
-				console.log(result["results"][0]["byline"]);
+				console.log(result["results"][4]["byline"]);
+
 				$("p#author1").text(result["results"][0]["byline"]);
 				$("p#author2").text(result["results"][1]["byline"]);
 				$("p#author3").text(result["results"][2]["byline"]);
 				$("p#author4").text(result["results"][3]["byline"]);
 				$("p#author5").text(result["results"][4]["byline"]);
 
-				  // var nextArticle = function(number) {
-				  // var name = result["results"][number]["name"];
-				  // console.log(name);
+				console.log(result["results"][0]["url"]);
+				var link1 = result["results"][0]["url"];
+				$("a#link1").attr("href", link1);
+				var link2 = result["results"][1]["url"];
+				$("a#link2").attr("href", link2);
+				var link3 = result["results"][2]["url"];
+				$("a#link3").attr("href", link3);
+				var link4 = result["results"][3]["url"];
+				$("a#link4").attr("href", link4);
+				var link5 = result["results"][4]["url"];
+				$("a#link5").attr("href", link5);
 
+				
+				console.log(result["results"][0]["multimedia"][0]["url"]);
 
-				//   var country = trip[number]["country"];
-				//   $("h1").text(city + ", " + country);
-				//   $("img").attr("src", trip[number]["photo"]);
-				// };
+				var photo1 = result["results"][0]["multimedia"][0]["url"];
+				$("img#photo1").attr("src", photo1);
+				var photo2 = result["results"][1]["multimedia"][0]["url"];
+				$("img#photo2").attr("src", photo2);
+				var photo3 = result["results"][2]["multimedia"][0]["url"];
+				$("img#photo3").attr("src", photo3);
+				var photo4 = result["results"][3]["multimedia"][0]["url"];
+				$("img#photo4").attr("src", photo4);
+				var photo5 = result["results"][4]["multimedia"][0]["url"];
+				$("img#photo5").attr("src", photo5);
 
-				// 	$("button#first").on("click", function() {
-				// 	  nextArticle(0);
-				// 	});
-				// 	$("button#second").on("click", function() {
-				// 	  nextAsrticle(1);
-				// 	});
-				// 	$("button#third").on("click", function() {
-				// 	  nextArticle(2);
-				// 	});
-
+				
 
 			}).fail(function(err) {
 			  throw err;
